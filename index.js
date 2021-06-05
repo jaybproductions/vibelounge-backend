97a805a021b9db5ee518605857eacb530f5257f0
jaybproductions <jaybgameproductions@gmail.com>
2021-06-04 22:33:54 -0400

    adding spotify login

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 
 const app = express();
-const PORT = 89;
+const PORT = 91;
+require("dotenv").config();
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("short"));
@@ -12,6 +13,19 @@ app.use(cors());
 
 app.get("/v1/api", (req, res) => {
   res.json({ message: "Welcome to the Vibe Lounge API" });
+});
+
+app.get("/login", function (req, res) {
+  var scopes = "user-read-private user-read-email streaming";
+  res.send(
+    "https://accounts.spotify.com/authorize" +
+      "?response_type=code" +
+      "&client_id=" +
+      process.env.SPOTIFY_CLIENT_ID +
+      (scopes ? "&scope=" + encodeURIComponent(scopes) : "") +
+      "&redirect_uri=" +
+      encodeURIComponent(process.env.SPOTIFY_REDIRECT_URI)
+  );
 });
 
 app.listen(PORT, () => {
